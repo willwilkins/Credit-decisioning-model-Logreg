2b75862e1c000e4e643fa6be3d8e6bc8c0bf763f
willwilkins <william.wilkins3452@gmail.com>
2025-10-05 11:42:38 +0100

    Tried to integrate univariate_multiplotter into init so it can be called with import src

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+"""Credit decisioning logistic regression project."""
+
+from .data_prep import (
+    load_uci_data,
+    preprocess_features,
+    split_data,
+    univariate_multiplotter
+)
+
+from .models import (
+    build_pipeline,
+    train_model,
+    plot_confusion_matrix,
+    plot_roc_curve
+)
+
+__version__ = "1.0.0"
